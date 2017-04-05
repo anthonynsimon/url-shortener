45068211bf94044fd2573126246c01ac69f24cd8
Anthony Najjar <anthonynsimon@gmail.com>
2017-04-05 09:19:41 +0200

    Rename redis key prefixes

--- api/src/main/scala/com/anthonynsimon/urlshortener/api/modules/ShortenUrlModule.scala
+++ api/src/main/scala/com/anthonynsimon/urlshortener/api/modules/ShortenUrlModule.scala
@@ -4,8 +4,8 @@ import com.twitter.inject.TwitterModule
 
 object ShortenUrlModule extends TwitterModule {
 
-	flag[String]("urls.keyprefix", "url-", "Default URL key prefix")
-	flag[String]("urls.reverselookupprefix", "revurl-", "Default URL reverse lookup key prefix")
+	flag[String]("urls.keyprefix", "url:", "Default URL key prefix")
+	flag[String]("urls.reverselookupprefix", "revurl:", "Default URL reverse lookup key prefix")
 	flag("urls.idcounterkey", "idcounter", "Default sequential ID counter database key")
 
 }
